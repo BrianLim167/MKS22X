14e39dae02b51fe15ec2cc1844da543855ee0ca1
BrianLim167 <blim167@gmail.com>
2017-03-06 15:34:37 -0500

    began writing scanner

---
+++ 05USACO/USACO.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*
+public class USACO{
+    public int bronze(String filename){
+	int[][] pasture;
+	int R,C,E,N;
+	Scanner scan = new Scanner(new File(filename));
+	if   (scan.hasNextLine){
+	    R = scan.next();
+	    C = scan.next();
+	    E = scan.next();
+	    N = scan.next();
+	}
+	pasture = new int[R][C];
+	while (scan.hasNextLine()){
+
+	}
+    }
+}

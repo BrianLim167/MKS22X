c1bec9f23a30306ec06562208f2e06a8ccf9c983
BrianLim167 <blim167@gmail.com>
2017-04-21 01:49:16 -0400

    wrote Postfix

---
+++ Postfix/Postfix.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+public class Postfix{
+    public static double eval(String exp){
+	String[] tokens = exp.split(" ");
+	Stack<Double> values = new Stack<Double>();
+	for (int ind=0 ; ind<tokens.length ; ind++){
+	    if (isOp(tokens[ind])){
+		values.push(apply(tokens[ind],values.pop(),values.pop()));
+	    }else{
+		values.push(Double.parseDouble(tokens[ind]));
+	    }
+	}
+	return values.pop();
+    }
+    private static boolean isOp(String token){
+	return token.length() == 1 && token.compareTo("0") < 0;
+    }
+    private static double apply(String op,double latter,double former){
+	if (op.equals("+")){
+	    return former+latter;
+	}else if (op.equals("-")){
+	    return former-latter;
+	}else if (op.equals("*")){
+	    return former*latter;
+	}else if (op.equals("/")){
+	    return former/latter;
+	}else if (op.equals("%")){
+	    return former%latter;
+	}
+	return 0;
+    }
+}
---
+++ Postfix/TestMyFile.java
@@ -0,0 +1,7 @@
+public class TestMyFile{
+    public static void main(String[]args){
+	System.out.println(Postfix.eval("10 2.0 +"));
+	System.out.println(Postfix.eval("11 3 - 4 + 2.5 *"));
+	System.out.println(Postfix.eval("8 2 + 99 9 - * 2 + 9 -"));
+    }
+}

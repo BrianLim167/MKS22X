bdff2af37d2e7cdae55f6dbf034e7d0e7c9894d5
BrianLim167 <blim167@gmail.com>
2017-05-15 15:31:34 -0400

    wrote Location

---
+++ 15MazeSolver/Location.java
@@ -0,0 +1,23 @@
+public class Location implements Comparable<Location>{
+    private int row,col;
+    private Location previous;
+    private int distToStart;
+    private int distToGoal;
+    private boolean aStar;
+
+    public Location (int row,int col,Location previous,int distToStart,int distToGoal,boolean aStar){
+	this.row = row;
+	this.col = col;
+	this.previous = previous;
+	this.distToStart = distToStart;
+	this.distToGoal = distToGoal;
+	this.aStar = aStar;
+    }
+    
+    public int compareTo(Location other){
+	if (aStar){
+	    return (distToStart+distToGoal)-(other.distToStart+other.distToGoal);
+	}
+	return distToGoal-other.distToGoal;
+    }
+}
